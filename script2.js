b82ce6f5165659f54c8ea186b3cb39107eb77759
tod619 <trevorodwyer619@hotmail.com>
2023-04-25 21:12:32 +0100

    test JSON.stringify in script.js file

--- script2.js
+++ script2.js
@@ -2,7 +2,8 @@ async function getQuestion() {
   const APIUrl = 'https://opentdb.com/api.php?amount=1';
   const result = await fetch(`${APIUrl}`);
   const data = await result.json();
-  console.log(data.results[0]);
-  console.log(data.results[0].correct_answer);
+  dataString = JSON.stringify(data);
+  //console.log(dataString);
+  //console.log(data.results[0].correct_answer);
 }
 getQuestion();
