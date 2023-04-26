dedaeabe7c9fb4477dd39c08d20be8fdbafd092e
tod619 <trevorodwyer619@hotmail.com>
2023-04-26 08:10:29 +0100

    Use script.js for javascript in index.html file

--- script2.js
+++ script2.js
@@ -2,8 +2,18 @@ async function getQuestion() {
   const APIUrl = 'https://opentdb.com/api.php?amount=1';
   const result = await fetch(`${APIUrl}`);
   const data = await result.json();
-  dataString = JSON.stringify(data);
+  //const dataString = JSON.parse(data);
+  //dataString = JSON.stringify(data);
   //console.log(dataString);
   //console.log(data.results[0].correct_answer);
+  //console.log(dataString);
+  console.log(data);
+
+  const question = {
+    questionText: `${data.results[0].question}`,
+    answerText: `${data.results[0].correct_answer}`,
+  };
+
+  console.log(question);
 }
 getQuestion();
